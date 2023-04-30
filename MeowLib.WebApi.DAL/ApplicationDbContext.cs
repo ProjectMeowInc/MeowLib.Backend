738b879a178bbca191d88dc879bb62618a355303
BlackSoulHub <mr.asaglaev@mail.ru>
2023-04-30 15:28:30 +0500

    Добавлена документация. Создание БД при создании контекста.

--- MeowLib.WebApi.DAL/ApplicationDbContext.cs
+++ MeowLib.WebApi.DAL/ApplicationDbContext.cs
@@ -3,12 +3,19 @@ using Microsoft.EntityFrameworkCore;
 
 namespace MeowLib.WebApi.DAL;
 
+/// <summary>
+/// Класс для работы EF.
+/// </summary>
 public class ApplicationDbContext : DbContext
 {
     public DbSet<UserEntityModel> Users { get; set; } = null!;
 
+    /// <summary>
+    /// Конструктор.
+    /// </summary>
+    /// <param name="options">Настройки БД.</param>
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
-        
+        Database.EnsureCreated();
     }
 }
