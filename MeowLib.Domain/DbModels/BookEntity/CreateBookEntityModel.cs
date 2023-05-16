bfe7c0868d31c0298d84f54e7b4c3ed9868bdd2a
BlackSoulHub <mr.asaglaev@mail.ru>
2023-05-16 20:21:19 +0500

    Сделан модель для создание книги

---
+++ MeowLib.Domain/DbModels/BookEntity/CreateBookEntityModel.cs
@@ -0,0 +1,7 @@
+namespace MeowLib.Domain.DbModels.BookEntity;
+
+public class CreateBookEntityModel
+{
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+}
