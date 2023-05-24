3bd22db48d49d6b8ba0174bc4e9ad05b70a0b4da
BlackSoulHub <mr.asaglaev@mail.ru>
2023-05-24 23:20:08 +0500

    Изменено возвращаемое значение

--- MeowLIb.WebApi.Services/Implementation/Production/TagService.cs
+++ MeowLIb.WebApi.Services/Implementation/Production/TagService.cs
@@ -109,9 +109,19 @@ public class TagService : ITagService
     /// </summary>
     /// <param name="id">Id тега.</param>
     /// <returns>Найденный тег, иначе - null</returns>
-    public async Task<TagEntityModel?> GetTagByIdAsync(int id)
+    public async Task<TagDto?> GetTagByIdAsync(int id)
     {
-        return await _tagRepository.GetByIdAsync(id);
+        var foundedTag = await _tagRepository.GetByIdAsync(id);
+        if (foundedTag is null)
+        {
+            return null;
+        }
+
+        return new TagDto
+        {
+            Id = foundedTag.Id,
+            Name = foundedTag.Name
+        };
     }
 
     /// <summary>
