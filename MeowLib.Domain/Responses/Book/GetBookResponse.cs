78e6280146d73cab7267d95100a4669f16eeb8bc
BlackSoulHub <mr.asaglaev@mail.ru>
2023-06-06 20:33:16 +0300

    Сделан ответ на получения книги

---
+++ MeowLib.Domain/Responses/Book/GetBookResponse.cs
@@ -0,0 +1,13 @@
+﻿using MeowLib.Domain.Dto.Author;
+using MeowLib.Domain.Dto.Tag;
+
+namespace MeowLib.Domain.Responses.Book;
+
+public class GetBookResponse
+{
+    public required int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public required AuthorDto Author { get; set; }
+    public required IEnumerable<TagDto> Tags { get; set; }
+}
