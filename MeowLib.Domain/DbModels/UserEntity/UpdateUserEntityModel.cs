0b5179bc0f769c9725a7766044aa602ff969bcd6
BlackSoulHub <mr.asaglaev@mail.ru>
2023-06-06 19:37:25 +0300

    Добавлено поле для обновления роли пользователя

--- MeowLib.Domain/DbModels/UserEntity/UpdateUserEntityModel.cs
+++ MeowLib.Domain/DbModels/UserEntity/UpdateUserEntityModel.cs
@@ -1,7 +1,10 @@
+using MeowLib.Domain.Enums;
+
 namespace MeowLib.Domain.DbModels.UserEntity;
 
 public class UpdateUserEntityModel
 {
     public string? Login { get; set; }
     public string? Password { get; set; }
+    public UserRolesEnum? Role { get; set; }
 }
--- MeowLib.Domain/Requests/User/UpdateUserRequest.cs
+++ MeowLib.Domain/Requests/User/UpdateUserRequest.cs
@@ -1,7 +1,10 @@
-﻿namespace MeowLib.Domain.Requests.User;
+﻿using MeowLib.Domain.Enums;
+
+namespace MeowLib.Domain.Requests.User;
 
 public class UpdateUserRequest
 {
     public string? Login { get; set; }
     public string? Password { get; set; }
+    public UserRolesEnum? Role { get; set; }
 }
