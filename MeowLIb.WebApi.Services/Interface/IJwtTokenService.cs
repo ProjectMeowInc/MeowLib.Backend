a15aa4435c9f7c5790b6a0ee9d02036eb82be698
BlackSoulHub <mr.asaglaev@mail.ru>
2023-06-07 23:16:28 +0300

    Создан метод для генерации `RefreshToken`

--- MeowLIb.WebApi.Services/Interface/IJwtTokenService.cs
+++ MeowLIb.WebApi.Services/Interface/IJwtTokenService.cs
@@ -1,4 +1,5 @@
 using MeowLib.Domain.Dto.User;
+using MeowLib.Domain.Models;
 
 namespace MeowLIb.WebApi.Services.Interface;
 
@@ -8,16 +9,31 @@ namespace MeowLIb.WebApi.Services.Interface;
 public interface IJwtTokenService
 {
     /// <summary>
-    /// Токен генерирует JWT-токен для авторизации пользователя.
+    /// Метод генерирует JWT-токен доступа.
     /// </summary>
     /// <param name="userData">Данные о пользователе.</param>
     /// <returns>JWT-токен в виде строки.</returns>
-    string GenerateToken(UserDto userData);
+    string GenerateAccessToken(UserDto userData);
+
+    /// <summary>
+    /// Метод генерирует JWT-токен обновления.
+    /// </summary>
+    /// <param name="userLogin">Логин пользователя к которому привязан данный токен.</param>
+    /// <param name="expiredAt">Время истечения токена обновления.</param>
+    /// <returns></returns>
+    string GenerateRefreshToken(string userLogin, DateTime expiredAt);
     
     /// <summary>
-    /// Парсит токен и возвращает информацию хранащуюся в нём.
+    /// Парсит JWT-токен доступа и возвращает информацию хранащуюся в нём.
     /// </summary>
     /// <param name="token">Токен.</param>
     /// <returns>Информация о пользователе в случае удачного парсинга, иначе - null</returns>
-    Task<UserDto?> ParseTokenAsync(string token);
+    Task<UserDto?> ParseAccessTokenAsync(string token);
+
+    /// <summary>
+    /// Метод парсит JWT-токет обновления и возвращает информацию хранащуюся в нём.
+    /// </summary>
+    /// <param name="token">Токен обновления.</param>
+    /// <returns>Информация в токене обновления.</returns>
+    Task<AccessTokenDataModel?> ParseRefreshTokenAsync(string token);
 }
