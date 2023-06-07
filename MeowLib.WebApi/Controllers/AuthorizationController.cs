ee0db1ac872b46e050eb9222af31a08bc379c0c6
BlackSoulHub <mr.asaglaev@mail.ru>
2023-06-07 23:14:07 +0300

    Методы авторизации вынесены в отдельный контроллер

---
+++ MeowLib.WebApi/Controllers/AuthorizationController.cs
@@ -0,0 +1,71 @@
+﻿using MeowLib.Domain.Exceptions;
+using MeowLib.Domain.Exceptions.Services;
+using MeowLib.Domain.Requests.User;
+using MeowLib.Domain.Responses;
+using MeowLib.Domain.Responses.User;
+using MeowLib.WebApi.Abstractions;
+using MeowLIb.WebApi.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeowLib.WebApi.Controllers;
+
+[Route("authorization")]
+public class AuthorizationController : BaseController
+{
+    private readonly IUserService _userService;
+
+    public AuthorizationController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpPost]
+    [Route("sign-in")]
+    [ProducesResponseType(200)]
+    [ProducesResponseType(400, Type = typeof(BaseErrorResponse))]
+    [ProducesResponseType(403, Type = typeof(ValidationErrorResponse))]
+    [ProducesResponseType(500, Type = typeof(BaseErrorResponse))]
+    public async Task<ActionResult> SignIn([FromBody] SignInRequest input)
+    {
+        var signInResult = await _userService.SignInAsync(input.Login, input.Password);
+
+        return signInResult.Match<ActionResult>(user => Json(user), exception =>
+        {
+            if (exception is ValidationException validationException)
+            {
+                return validationException.ToResponse();
+            }
+
+            if (exception is ApiException)
+            {
+                return Error("Пользователь с таким логином уже занят", 400);
+            }
+
+            return ServerError();
+        });
+    }
+
+    [HttpPost]
+    [Route("log-in")]
+    [ProducesResponseType(200, Type = typeof(LogInResponse))]
+    [ProducesResponseType(401, Type = typeof(BaseErrorResponse))]
+    [ProducesResponseType(500, Type = typeof(BaseErrorResponse))]
+    public async Task<ActionResult> LogIn([FromBody] LogInRequest input)
+    {
+        var logInResult = await _userService.LogIn(input.Login, input.Password, input.LongSession);
+
+        return logInResult.Match<ActionResult>(tokens => Json(new LogInResponse
+        {
+            AccessToken = tokens.accessToken,
+            RefreshToken = tokens.refreshToken
+        }), exception =>
+        {
+            if (exception is IncorrectCreditionalException)
+            {
+                return Error("Неверный логин или пароль", 401);
+            }
+
+            return ServerError();
+        });
+    }
+}
