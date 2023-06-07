d0bd50f15d1c3058b0fc285947ec9d005522b22a
BlackSoulHub <mr.asaglaev@mail.ru>
2023-06-07 23:11:09 +0300

    Сделано исключение для некорректных авторизационных данных

---
+++ MeowLib.Domain/Exceptions/Services/IncorrectCreditionalException.cs
@@ -0,0 +1,9 @@
+﻿namespace MeowLib.Domain.Exceptions.Services;
+
+public class IncorrectCreditionalException : ApiException
+{
+    public IncorrectCreditionalException(string errorMessage) : base(errorMessage)
+    {
+        
+    }
+}
