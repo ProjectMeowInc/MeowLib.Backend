7d39d45f416f2dfeb6dfeaa1752167f4dab5c112
BlackSoulHub <mr.asaglaev@mail.ru>
2023-06-08 16:49:04 +0300

    Добавлена документация

--- MeowLib.Domain/Exceptions/Services/IncorrectCreditionalException.cs
+++ MeowLib.Domain/Exceptions/Services/IncorrectCreditionalException.cs
@@ -1,5 +1,8 @@
 ﻿namespace MeowLib.Domain.Exceptions.Services;
 
+/// <summary>
+/// Класс описывающий исключения для некорректных авторизационные данных.
+/// </summary>
 public class IncorrectCreditionalException : ApiException
 {
     public IncorrectCreditionalException(string errorMessage) : base(errorMessage)
