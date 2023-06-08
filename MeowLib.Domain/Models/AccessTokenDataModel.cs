620066b6cf84fc7951fe9f2f02a62a7001c271d5
BlackSoulHub <mr.asaglaev@mail.ru>
2023-06-08 16:33:33 +0300

    Добавлена документация

--- MeowLib.Domain/Models/AccessTokenDataModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace MeowLib.Domain.Models;
-
-public class AccessTokenDataModel
-{
-    public required string Login { get; set; }
-}
---
+++ MeowLib.Domain/Models/RefreshTokenDataModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MeowLib.Domain.Models;
+
+/// <summary>
+/// Класс описывает данные, хранящиеся в JWT-токене для обновления.
+/// </summary>
+public class RefreshTokenDataModel
+{
+    /// <summary>
+    /// Логин пользователя-владельца токена.
+    /// </summary>
+    public required string Login { get; set; }
+}
