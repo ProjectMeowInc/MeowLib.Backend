4a77051b212d89fd3624316aed8bdb310b5440b8
BlackSoulHub <mr.asaglaev@mail.ru>
2023-06-11 15:28:24 +0300

    `ChapterRepository` и `ChapterService` добавлены в DI

--- MeowLib.WebApi/Program.cs
+++ MeowLib.WebApi/Program.cs
@@ -56,6 +56,7 @@ services.AddScoped<IUserRepository, UserRepository>();
 services.AddScoped<IAuthorRepository, AuthorRepository>();
 services.AddScoped<ITagRepository, TagRepository>();
 services.AddScoped<IBookRepository, BookRepository>();
+services.AddScoped<IChapterRepository, ChapterRepository>();
 
 // Init services
 services.AddSingleton<IHashService, HashService>();
@@ -66,6 +67,7 @@ services.AddScoped<IUserService, UserService>();
 services.AddScoped<IAuthorService, AuthorService>();
 services.AddScoped<ITagService, TagService>();
 services.AddScoped<IBookService, BookService>();
+services.AddScoped<IChapterService, ChapterService>();
 
 builder.Services.AddDbContext<ApplicationDbContext>(dbOptions =>
 {
