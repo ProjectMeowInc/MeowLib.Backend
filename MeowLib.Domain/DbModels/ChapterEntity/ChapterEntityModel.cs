36ec0e36bf65a9b10f2e97db6eae747af52dd47b
BlackSoulHub <mr.asaglaev@mail.ru>
2023-06-11 14:10:31 +0300

    Создана модель для глав

---
+++ MeowLib.Domain/DbModels/ChapterEntity/ChapterEntityModel.cs
@@ -0,0 +1,12 @@
+﻿using MeowLib.Domain.DbModels.BookEntity;
+
+namespace MeowLib.Domain.DbModels.ChapterEntity;
+
+public class ChapterEntityModel
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Text { get; set; }
+    public required DateTime ReleaseDate { get; set; }
+    public required BookEntityModel Book { get; set; }
+}
