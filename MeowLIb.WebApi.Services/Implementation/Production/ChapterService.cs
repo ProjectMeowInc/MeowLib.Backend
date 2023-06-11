9c71a93b783b159e00de91d464031b6614a8d3a8
BlackSoulHub <mr.asaglaev@mail.ru>
2023-06-11 15:29:49 +0300

    Сделана основа для сервиса глав

---
+++ MeowLIb.WebApi.Services/Implementation/Production/ChapterService.cs
@@ -0,0 +1,59 @@
+﻿using LanguageExt.Common;
+using MeowLib.Domain.DbModels.BookEntity;
+using MeowLib.Domain.DbModels.ChapterEntity;
+using MeowLib.Domain.Exceptions.DAL;
+using MeowLib.WebApi.DAL.Repository.Interfaces;
+using MeowLIb.WebApi.Services.Interface;
+
+namespace MeowLIb.WebApi.Services.Implementation.Production;
+
+public class ChapterService : IChapterService
+{
+    private readonly IChapterRepository _chapterRepository;
+    private readonly IBookService _bookService;
+    
+    public ChapterService(IChapterRepository chapterRepository, IBookService bookService)
+    {
+        _chapterRepository = chapterRepository;
+        _bookService = bookService;
+    }
+    
+    /// <summary>
+    /// Метод создаёт новую главу.
+    /// </summary>
+    /// <param name="name">Название главы.</param>
+    /// <param name="text">Текст главы.</param>
+    /// <param name="bookId">Id книги.</param>
+    /// <returns>Модель созданной главы.</returns>
+    /// <exception cref="EntityNotFoundException">Возникает в случае, если книга не была найдена.</exception>
+    /// <exception cref="DbSavingException">Возникает в случае ошибки БД.</exception>
+    public async Task<Result<ChapterEntityModel>> CreateChapterAsync(string name, string text, int bookId)
+    {
+        var foundedBook = await _bookService.GetBookByIdAsync(bookId);
+        if (foundedBook is null)
+        {
+            var entityNotFoundException = new EntityNotFoundException(nameof(BookEntityModel), $"Id={bookId}");
+            return new Result<ChapterEntityModel>(entityNotFoundException);
+        }
+
+        var newChapter = new ChapterEntityModel
+        {
+            Name = name,
+            Text = text,
+            ReleaseDate = DateTime.UtcNow,
+            Book = foundedBook
+        };
+
+        var createChapterResult = await _chapterRepository.CreateAsync(newChapter);
+        return createChapterResult.Match<Result<ChapterEntityModel>>(createdChapter => createdChapter, exception =>
+        {
+            if (exception is DbSavingException dbSavingException)
+            {
+                return new Result<ChapterEntityModel>(dbSavingException);
+            }
+
+            // TODO: Add logs
+            return new Result<ChapterEntityModel>(exception); 
+        });
+    }
+}
---
+++ MeowLIb.WebApi.Services/Interface/IChapterService.cs
@@ -0,0 +1,19 @@
+﻿using LanguageExt.Common;
+using MeowLib.Domain.DbModels.ChapterEntity;
+using MeowLib.Domain.Exceptions.DAL;
+
+namespace MeowLIb.WebApi.Services.Interface;
+
+public interface IChapterService
+{
+    /// <summary>
+    /// Метод создаёт новую главу.
+    /// </summary>
+    /// <param name="name">Название главы.</param>
+    /// <param name="text">Текст главы.</param>
+    /// <param name="bookId">Id книги.</param>
+    /// <returns>Модель созданной главы.</returns>
+    /// <exception cref="EntityNotFoundException">Возникает в случае, если книга не была найдена.</exception>
+    /// <exception cref="DbSavingException">Возникает в случае ошибки БД.</exception>
+    Task<Result<ChapterEntityModel>> CreateChapterAsync(string name, string text, int bookId);
+}
