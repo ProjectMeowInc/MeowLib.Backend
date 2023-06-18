fd12e892e5e8127355b8f876e5557e4fe72aa32f
BlackSoulHub <mr.asaglaev@mail.ru>
2023-06-18 21:02:36 +0300

    Сделан ответ на полученияе главы книги

---
+++ MeowLib.Domain/Responses/Chapter/GetBookChapterResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace MeowLib.Domain.Responses.Chapter;
+
+public class GetBookChapterResponse
+{
+    public required int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Text { get; set; }
+    public required DateTime ReleaseDate { get; set; }
+}
