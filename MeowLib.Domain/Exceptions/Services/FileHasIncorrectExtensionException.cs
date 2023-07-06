126a3d33d262cd43a1bb8098d3961c45bf606cb2
BlackSoulHub <mr.asaglaev@mail.ru>
2023-07-06 09:04:32 +0300

    Сделана исключение для случае, когда файл имеет некорректное расширение

---
+++ MeowLib.Domain/Exceptions/Services/FileHasIncorrectExtensionException.cs
@@ -0,0 +1,11 @@
+﻿namespace MeowLib.Domain.Exceptions.Services;
+
+public class FileHasIncorrectExtensionException : ApiException
+{
+    public string CurrentException { get; protected set; }
+    
+    public FileHasIncorrectExtensionException(string errorMessage, string currentExtension) : base(errorMessage)
+    {
+        CurrentException = currentExtension;
+    }
+}
