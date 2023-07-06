1726bdf40bdc9d92db36ac8bfbb8a2fa41f0fd22
BlackSoulHub <mr.asaglaev@mail.ru>
2023-07-06 17:24:24 +0300

    Сделана основа контроллера для работы со списком книг пользователя

---
+++ MeowLib.WebApi/Controllers/FavoriteController.cs
@@ -0,0 +1,10 @@
+﻿using MeowLib.WebApi.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeowLib.WebApi.Controllers;
+
+[Route("api/users/favorite")]
+public class FavoriteController : BaseController
+{
+    
+}
