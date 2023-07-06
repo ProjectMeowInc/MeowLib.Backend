f218d50f23de99dd571ef92e3c9d58d9008a048c
BlackSoulHub <mr.asaglaev@mail.ru>
2023-07-06 18:02:22 +0300

    Сделан сервис для работы со списком книг пользователя

---
+++ MeowLIb.WebApi.Services/Interface/IUserFavoriteService.cs
@@ -0,0 +1,19 @@
+﻿using LanguageExt.Common;
+using MeowLib.Domain.DbModels.UserFavoriteEntity;
+using MeowLib.Domain.Enums;
+
+namespace MeowLIb.WebApi.Services.Interface;
+
+public interface IUserFavoriteService
+{
+    /// <summary>
+    /// Метод обновляет книгу в списке пользователя.
+    /// </summary>
+    /// <param name="bookId">Id книги.</param>
+    /// <param name="userId">Id пользователя.</param>
+    /// <param name="status">Статус для обновления</param>
+    /// <returns>Обновлённую книгу в списке пользователя.</returns>
+    /// <exception cref="EntityNotFoundException">Возникает в случае, если книга или пользователь не были найдены.</exception>
+    Task<Result<UserFavoriteEntityModel>> AddOrUpdateUserListAsync(int bookId, int userId,
+        UserFavoritesStatusEnum status);
+}
