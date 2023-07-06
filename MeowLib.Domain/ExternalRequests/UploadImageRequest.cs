c2a2a6c74dd8650f932184a6b5c49957ca308cfe
BlackSoulHub <mr.asaglaev@mail.ru>
2023-07-06 09:05:12 +0300

    Сделан запрос/ответ на загрузку картинок

---
+++ MeowLib.Domain/ExternalRequests/UploadImageRequest.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace MeowLib.Domain.ExternalRequests;
+
+public class UploadImageRequest
+{
+    [JsonProperty(PropertyName = "key")]
+    public required string Key { get; init; }
+    
+    [JsonProperty(PropertyName = "source")]
+    public required string Source { get; init; }
+}
---
+++ MeowLib.Domain/ExternalResponses/UploadImageResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MeowLib.Domain.ExternalResponses;
+
+public class UploadImageResponse
+{
+    [JsonPropertyName("status_code")]
+    public required int StatusCode { get; init; }
+    
+    [JsonPropertyName("url")]
+    public string? Url { get; init; }
+}
