d70d7f4503bf6f4c5f1687456891c68db163bf1e
BlackSoulHub <mr.asaglaev@mail.ru>
2023-07-06 12:35:55 +0300

    Сделан контроллер для получения картинок

---
+++ MeowLib.WebApi/Controllers/FileController.cs
@@ -0,0 +1,29 @@
+﻿using MeowLib.WebApi.Abstractions;
+using MeowLIb.WebApi.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeowLib.WebApi.Controllers;
+
+[ApiController]
+[Route("api/images")]
+public class FileController : BaseController
+{
+    private readonly IUploadFileService _uploadFileService;
+    
+    public FileController(IUploadFileService uploadFileService)
+    {
+        _uploadFileService = uploadFileService;
+    }
+
+    [HttpGet("book/{imageName}")]
+    public async Task<ActionResult> GetBookImage([FromRoute] string imageName)
+    {
+        var getBookImageResult = await _uploadFileService.GetBookImageAsync(imageName);
+        if (getBookImageResult.content is null)
+        {
+            return NotFoundError("Изображение не найдено");
+        }
+        
+        return File(getBookImageResult.content, getBookImageResult.mimeType);
+    }
+}
