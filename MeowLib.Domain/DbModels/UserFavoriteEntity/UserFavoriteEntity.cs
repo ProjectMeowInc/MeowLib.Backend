a0f77a6aa662ef303e11d54087dbe2e26b278d6f
BlackSoulHub <mr.asaglaev@mail.ru>
2023-07-06 17:23:57 +0300

    Сделана модель списка книг пользователя

---
+++ MeowLib.Domain/DbModels/UserFavoriteEntity/UserFavoriteEntity.cs
@@ -0,0 +1,28 @@
+﻿using MeowLib.Domain.DbModels.BookEntity;
+using MeowLib.Domain.DbModels.UserEntity;
+using MeowLib.Domain.Enums;
+
+namespace MeowLib.Domain.DbModels.UserFavoriteEntity;
+
+public class UserFavoriteEntity
+{
+    /// <summary>
+    /// Id.
+    /// </summary>
+    public int Id { get; set; }
+    
+    /// <summary>
+    /// Ссылка на книгу.
+    /// </summary>
+    public required BookEntityModel Book { get; set; }
+    
+    /// <summary>
+    /// Ссылка на пользователя.
+    /// </summary>
+    public required UserEntityModel User { get; set; }
+    
+    /// <summary>
+    /// Статус.
+    /// </summary>
+    public required UserFavoritesStatusEnum Status { get; set; }
+}
