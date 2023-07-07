3d013e665265dac12095aeb1f7bb5364cac0c520
BlackSoulHub <mr.asaglaev@mail.ru>
2023-07-07 11:58:49 +0300

    Исправления в `using`

--- MeowLib.WebApi.DAL/Repository/Interfaces/IUserFavoriteRepository.cs
+++ MeowLib.WebApi.DAL/Repository/Interfaces/IUserFavoriteRepository.cs
@@ -1,8 +1,8 @@
-﻿using LanguageExt;
-using LanguageExt.Common;
+﻿using LanguageExt.Common;
 using MeowLib.Domain.DbModels.BookEntity;
 using MeowLib.Domain.DbModels.UserEntity;
 using MeowLib.Domain.DbModels.UserFavoriteEntity;
+using MeowLib.Domain.Exceptions.DAL;
 
 namespace MeowLib.WebApi.DAL.Repository.Interfaces;
 
