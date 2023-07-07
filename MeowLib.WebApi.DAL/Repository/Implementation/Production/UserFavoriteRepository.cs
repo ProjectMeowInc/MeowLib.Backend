ff21975cc5d3f9eabb2d9f404238838020b23102
BlackSoulHub <mr.asaglaev@mail.ru>
2023-07-07 11:54:59 +0300

    Добавлена документация для сервиса книг пользователей

--- MeowLib.WebApi.DAL/Repository/Implementation/Production/UserFavoriteRepository.cs
+++ MeowLib.WebApi.DAL/Repository/Implementation/Production/UserFavoriteRepository.cs
@@ -10,15 +10,28 @@ using Microsoft.EntityFrameworkCore;
 
 namespace MeowLib.WebApi.DAL.Repository.Implementation.Production;
 
+/// <summary>
+/// Репозиторий для работы с книгами пользователей.
+/// </summary>
 public class UserFavoriteRepository : IUserFavoriteRepository
 {
     private readonly ApplicationDbContext _applicationDbContext;
 
+    /// <summary>
+    /// Конструктор.
+    /// </summary>
+    /// <param name="applicationDbContext">Контекст БД.</param>
     public UserFavoriteRepository(ApplicationDbContext applicationDbContext)
     {
         _applicationDbContext = applicationDbContext;
     }
 
+    /// <summary>
+    /// Метод создаёт новую книгу в списке пользователя.
+    /// </summary>
+    /// <param name="entityModel">Модель для создания.</param>
+    /// <returns>Созданную книгу в списке пользователя.</returns>
+    /// <exception cref="DbSavingException">Возникает в случае ошибки сохранения данных.</exception>
     public async Task<Result<UserFavoriteEntityModel>> CreateAsync(UserFavoriteEntityModel entityModel)
     {
         var createdUserFavoritesEntry = await _applicationDbContext.UsersFavorite.AddAsync(entityModel);
@@ -36,16 +49,32 @@ public class UserFavoriteRepository : IUserFavoriteRepository
         return createdUserFavoritesEntry.Entity;
     }
 
+    /// <summary>
+    /// Метод получает книгу пользователя по Id.
+    /// </summary>
+    /// <param name="id">Id книги.</param>
+    /// <returns>Найденную книгу или null, если её нет.</returns>
     public async Task<UserFavoriteEntityModel?> GetByIdAsync(int id)
     {
         return await _applicationDbContext.UsersFavorite.FirstOrDefaultAsync(uf => uf.Id == id);
     }
 
+    /// <summary>
+    /// Метод получает книгу в списке пользователя по книгу и пользователю.
+    /// </summary>
+    /// <param name="book">Книга.</param>
+    /// <param name="user">Пользователь.</param>
+    /// <returns>Найденную книгу или null, если её нет.</returns>
     public async Task<UserFavoriteEntityModel?> GetByBookAndUserAsync(BookEntityModel book, UserEntityModel user)
     {
         return await _applicationDbContext.UsersFavorite.FirstOrDefaultAsync(uf => uf.Book == book && uf.User == user);
     }
 
+    /// <summary>
+    /// Метод удаляет книгу в списке пользователя.
+    /// </summary>
+    /// <param name="entityModel">Книга в списке пользователя для удаления</param>
+    /// <returns>True - в случае успеха, иначе - false.</returns>
     public async Task<bool> DeleteAsync(UserFavoriteEntityModel entityModel)
     {
         _applicationDbContext.UsersFavorite.Remove(entityModel);
@@ -62,6 +91,12 @@ public class UserFavoriteRepository : IUserFavoriteRepository
         return true;
     }
 
+    /// <summary>
+    /// Метод обновляет книгу в спике пользователя.
+    /// </summary>
+    /// <param name="entityModel">Книга в списке пользователя для обновления.</param>
+    /// <returns>Обновлённую модель книги</returns>
+    /// <exception cref="DbSavingException">Возникает в случае ошибки сохранения данных.</exception>
     public async Task<Result<UserFavoriteEntityModel>> UpdateAsync(UserFavoriteEntityModel entityModel)
     {
         var updateUserFavoriteEntry = _applicationDbContext.UsersFavorite.Update(entityModel);
@@ -79,5 +114,9 @@ public class UserFavoriteRepository : IUserFavoriteRepository
         return updateUserFavoriteEntry.Entity;
     }
 
+    /// <summary>
+    /// Метод получает все книги в списке пользователя в виде <see cref="IQueryable{T}"/>
+    /// </summary>
+    /// <returns></returns>
     public IQueryable<UserFavoriteEntityModel> GetAll() => _applicationDbContext.UsersFavorite.AsQueryable();
 }
