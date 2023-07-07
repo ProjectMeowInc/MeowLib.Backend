8ac7006e90b2360c5c2026ed63aad366d08b2808
BlackSoulHub <mr.asaglaev@mail.ru>
2023-07-07 20:53:22 +0300

    Сделаны поддерживаемые типы ответов для удобства/уменьшения дублирования кода

---
+++ MeowLib.WebApi/ProducesResponseTypes/ProducesServerErrorResponseTypeAttribute.cs
@@ -0,0 +1,15 @@
+﻿using MeowLib.Domain.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeowLib.WebApi.ProducesResponseTypes;
+
+public class ProducesServerErrorResponseTypeAttribute : ProducesResponseTypeAttribute
+{
+    public ProducesServerErrorResponseTypeAttribute() : base(typeof(BaseErrorResponse), 500)
+    {
+    }
+
+    public ProducesServerErrorResponseTypeAttribute(Type type, int statusCode, string contentType, params string[] additionalContentTypes) : base(type, statusCode, contentType, additionalContentTypes)
+    {
+    }
+}
