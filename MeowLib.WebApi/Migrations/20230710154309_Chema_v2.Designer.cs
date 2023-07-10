74f8d4e12a5fe633b13246e7b76c2d71bbe40e87
BlackSoulHub <mr.asaglaev@mail.ru>
2023-07-10 18:43:50 +0300

    Сделана миграцию на `v2`

--- MeowLib.WebApi/Migrations/20230710154309_Chema_v2.Designer.cs
+++ MeowLib.WebApi/Migrations/20230710154309_Chema_v2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using MeowLib.WebApi.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MeowLib.WebApi.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230710154309_Chema_v2")]
+    partial class Chema_v2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.5");
---
+++ MeowLib.WebApi/Migrations/20230710154309_Chema_v2.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MeowLib.WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Chema_v2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Authors_AuthorId",
+                table: "Books");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Books",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "UsersFavorite",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    BookId = table.Column<int>(type: "INTEGER", nullable: false),
+                    UserId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Status = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsersFavorite", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UsersFavorite_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UsersFavorite_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsersFavorite_BookId",
+                table: "UsersFavorite",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsersFavorite_UserId",
+                table: "UsersFavorite",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Authors_AuthorId",
+                table: "Books",
+                column: "AuthorId",
+                principalTable: "Authors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Authors_AuthorId",
+                table: "Books");
+
+            migrationBuilder.DropTable(
+                name: "UsersFavorite");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Books");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Authors_AuthorId",
+                table: "Books",
+                column: "AuthorId",
+                principalTable: "Authors",
+                principalColumn: "Id");
+        }
+    }
+}
