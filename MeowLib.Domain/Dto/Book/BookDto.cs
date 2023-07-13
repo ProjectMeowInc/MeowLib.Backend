85c9df06535320fae6bc00b39d6e4531d6d6be65
BlackSoulHub <mr.asaglaev@mail.ru>
2023-07-13 12:55:08 +0300

    Добавлено поле `ImageName` для `BookDto`

--- MeowLib.Domain/Dto/Book/BookDto.cs
+++ MeowLib.Domain/Dto/Book/BookDto.cs
@@ -5,4 +5,5 @@ public class BookDto
     public required int Id { get; set; }
     public required string Name { get; set; }
     public required string Description { get; set; }
+    public required string? ImageName { get; set; }
 }
