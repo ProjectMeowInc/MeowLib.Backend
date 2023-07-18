a8f1aa54f94cba998aae7724c386e0f9f956c745
BlackSoulHub <mr.asaglaev@mail.ru>
2023-07-18 17:12:10 +0300

    Сделано исключение для ситуаций когда книга не была найдена

---
+++ MeowLib.Domain/Exceptions/Book/BookNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace MeowLib.Domain.Exceptions.Book;
+
+public class BookNotFoundException : ApiException
+{
+    public BookNotFoundException(int bookId) : base($"Книга с Id = {bookId} не найдена")
+    {
+    }
+}
