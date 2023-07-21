1af9bd826346c7c1904f5e785de7d84e341d7e1f
BlackSoulHub <mr.asaglaev@mail.ru>
2023-07-21 05:43:01 +0300

    Сделан запрос на получение комментариев книги

---
+++ MeowLib.Domain/Responses/BookComment/GetBookCommentsResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+using MeowLib.Domain.Dto.BookComment;
+
+namespace MeowLib.Domain.Responses.BookComment;
+
+public class GetBookCommentsResponse
+{
+    [JsonPropertyName("bookId")]
+    public required int BookId { get; set; }
+    
+    [JsonPropertyName("items")]
+    public required IEnumerable<BookCommentDto> Items { get; set; }
+}
