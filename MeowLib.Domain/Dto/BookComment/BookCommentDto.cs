c1543327a6ca43a744d9e797376916b2f0d22dfb
BlackSoulHub <mr.asaglaev@mail.ru>
2023-07-21 05:31:04 +0300

    Сделана DTO-модель `BookCommentDto`

---
+++ MeowLib.Domain/Dto/BookComment/BookCommentDto.cs
@@ -0,0 +1,11 @@
+﻿using MeowLib.Domain.Dto.User;
+
+namespace MeowLib.Domain.Dto.BookComment;
+
+public class BookCommentDto
+{
+    public required int Id { get; set; }
+    public required string Text { get; set; }
+    public required DateTime PostedAt { get; set; }
+    public required UserDto Author { get; set; }
+}
