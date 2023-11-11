9addf106677bb339703745afccb8a4cac9661cd1
BlackSoulHub <mr.asaglaev@mail.ru>
2023-11-11 18:08:41 +0500

    create enum for team member role

---
+++ MeowLib.Domain/Enums/UserTeamMemberRoleEnum.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MeowLib.Domain.Enums;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum UserTeamMemberRoleEnum
+{
+    Translator = 1,
+    Redactor = 2,
+    Admin = 1000
+}
