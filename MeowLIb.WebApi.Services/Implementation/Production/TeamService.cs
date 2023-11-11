9c3de8fc59fdedd4adebdb1a9a152cd2b0afe3d7
BlackSoulHub <mr.asaglaev@mail.ru>
2023-11-11 18:06:04 +0500

    create service for team logic

---
+++ MeowLIb.WebApi.Services/Implementation/Production/TeamService.cs
@@ -0,0 +1,64 @@
+﻿using MeowLib.Domain.DbModels.TeamEntity;
+using MeowLib.Domain.DbModels.TeamMemberEntity;
+using MeowLib.Domain.Enums;
+using MeowLib.Domain.Exceptions.Team;
+using MeowLib.Domain.Result;
+using MeowLib.WebApi.DAL;
+using MeowLib.WebApi.DAL.Repository.Interfaces;
+using MeowLIb.WebApi.Services.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeowLIb.WebApi.Services.Implementation.Production;
+
+public class TeamService : ITeamService
+{
+    private readonly ApplicationDbContext _dbContext;
+    private readonly IUserRepository _userRepository;
+    
+    public TeamService(ApplicationDbContext dbContext, IUserRepository userRepository)
+    {
+        _dbContext = dbContext;
+        _userRepository = userRepository;
+    }
+
+    public async Task<Result<TeamEntityModel>> CreateNewTeamAsync(int createdById, string name, string description)
+    {
+        var foundedOwner = await _userRepository.GetByIdAsync(createdById);
+        if (foundedOwner is null)
+        {
+            return Result<TeamEntityModel>.Fail(new TeamOwnerNotFoundException(createdById));
+        }
+        
+        // create team
+        var createdEntry = await _dbContext.Teams.AddAsync(new TeamEntityModel
+        {
+            Name = name,
+            Description = description,
+            Members = new List<TeamMemberEntityModel>(),
+            Owner = foundedOwner
+        });
+        
+        // add owner to team
+        var createdTeam = createdEntry.Entity;
+        createdTeam.Members.Add(new TeamMemberEntityModel
+        {
+            User = foundedOwner,
+            Team = createdTeam,
+            Role = UserMemberTeamRoleEnum.Admin
+        });
+
+        _dbContext.Update(createdTeam);
+        await _dbContext.SaveChangesAsync();
+        
+        return Result<TeamEntityModel>.Ok(createdEntry.Entity);
+    }
+
+    public async Task<TeamEntityModel?> GetTeamByIdAsync(int teamId)
+    {
+        return await _dbContext.Teams
+            .Include(t => t.Members)
+            .ThenInclude(t => t.User)
+            .Include(t => t.Owner)
+            .FirstOrDefaultAsync(t => t.Id == teamId);
+    }
+}
---
+++ MeowLIb.WebApi.Services/Interface/ITeamService.cs
@@ -0,0 +1,25 @@
+﻿using MeowLib.Domain.DbModels.TeamEntity;
+using MeowLib.Domain.Exceptions.Team;
+using MeowLib.Domain.Result;
+
+namespace MeowLIb.WebApi.Services.Interface;
+
+public interface ITeamService
+{
+    /// <summary>
+    /// Метод создаёт новую команду.
+    /// </summary>
+    /// <param name="createdById">Id пользователя запросившего создание команды.</param>
+    /// <param name="name">Название команды.</param>
+    /// <param name="description">Описание команды.</param>
+    /// <returns>Модель команды в случае успеха</returns>
+    /// <exception cref="TeamOwnerNotFoundException">Возникает в случае, если пользователь запросивший создание команды не найден</exception>
+    Task<Result<TeamEntityModel>> CreateNewTeamAsync(int createdById, string name, string description);
+    
+    /// <summary>
+    /// Метод вовзращает информацию о команде по её Id.
+    /// </summary>
+    /// <param name="teamId">Id команды.</param>
+    /// <returns>Модель команды, если она была найдена. Иначе - null</returns>
+    Task<TeamEntityModel?> GetTeamByIdAsync(int teamId);
+}
