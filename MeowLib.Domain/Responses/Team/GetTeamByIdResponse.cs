b119a98d8d0a6ba24350fb06e1d65fa005dd02c8
BlackSoulHub <mr.asaglaev@mail.ru>
2023-11-11 18:09:56 +0500

    created response for getting team by id

---
+++ MeowLib.Domain/Responses/Team/GetTeamByIdResponse.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+using MeowLib.Domain.Enums;
+
+namespace MeowLib.Domain.Responses.Team;
+
+public class GetTeamByIdResponse
+{
+    [JsonPropertyName("id")]
+    public required int Id { get; init; }
+    [JsonPropertyName("name")]
+    public required string Name { get; init; }
+    [JsonPropertyName("description")]
+    public required string Description { get; init; }
+    [JsonPropertyName("members")]
+    public required IEnumerable<TeamMember> Members { get; init; }
+}
+
+public class TeamMember
+{
+    [JsonPropertyName("id")]
+    public required int Id { get; init; }
+    [JsonPropertyName("login")]
+    public required string Login { get; init; }
+    [JsonPropertyName("role")]
+    public required UserTeamMemberRoleEnum Role { get; init; }
+}
