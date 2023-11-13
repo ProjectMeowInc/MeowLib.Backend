c2b927728b725bb57a82fdbc4e8b0bbbe1cdb7ef
BlackSoulHub <mr.asaglaev@mail.ru>
2023-11-13 21:39:06 +0500

    create notification model

---
+++ MeowLib.Domain/DbModels/NotificationEntity/NotificationEntityModel.cs
@@ -0,0 +1,15 @@
+﻿using MeowLib.Domain.DbModels.NotificationEntity.Payload;
+using MeowLib.Domain.DbModels.UserEntity;
+using MeowLib.Domain.Enums;
+
+namespace MeowLib.Domain.DbModels.NotificationEntity;
+
+public class NotificationEntityModel
+{
+    public int Id { get; init; }
+    public required NotificationTypeEnum Type { get; init; }
+    public required BaseNotificationPayload Payload { get; init; }
+    public required DateTime CreatedAt { get; init; }
+    public required bool IsWatched { get; set; }
+    public required UserEntityModel User { get; init; }
+}
