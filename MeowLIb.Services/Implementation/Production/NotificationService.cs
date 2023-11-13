c0d1763cec97b27627c6b0257d61e741e4caf3a8
BlackSoulHub <mr.asaglaev@mail.ru>
2023-11-13 21:38:57 +0500

    create base notification service

---
+++ MeowLIb.Services/Implementation/Production/NotificationService.cs
@@ -0,0 +1,30 @@
+﻿using MeowLib.DAL;
+using MeowLib.Domain.Dto.Notification;
+using MeowLib.Domain.Result;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeowLib.Services.Implementation.Production;
+
+public class NotificationService
+{
+    private readonly ApplicationDbContext _dbContext;
+    
+    public NotificationService(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Result<IEnumerable<NotificationDto>>> GetUserNotifications(int userId)
+    {
+        return await _dbContext.Notifications
+            .Where(n => n.User.Id == userId && n.IsWatched == false)
+            .Select(n => new NotificationDto
+            {
+                Id = n.Id,
+                CreatedAt = n.CreatedAt,
+                Payload = n.Payload,
+                Type = n.Type
+            })
+            .ToListAsync();
+    }
+}
