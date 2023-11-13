7278015e48f20ba9c042adb0f868dd5b578025a5
BlackSoulHub <mr.asaglaev@mail.ru>
2023-11-13 21:37:46 +0500

    create DTO model for notifications

---
+++ MeowLib.Domain/Dto/Notification/NotificationDto.cs
@@ -0,0 +1,12 @@
+﻿using MeowLib.Domain.DbModels.NotificationEntity.Payload;
+using MeowLib.Domain.Enums;
+
+namespace MeowLib.Domain.Dto.Notification;
+
+public class NotificationDto
+{
+    public required int Id { get; init; }
+    public required NotificationTypeEnum Type { get; init; }
+    public required BaseNotificationPayload Payload { get; init; }
+    public required DateTime CreatedAt { get; init; }
+}
