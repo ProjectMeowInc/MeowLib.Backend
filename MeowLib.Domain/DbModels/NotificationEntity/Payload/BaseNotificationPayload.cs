0f2aa4703a7d0a78cbf40c1feaa67b536dfd6e45
blacksoulhub <mr.asaglaev@mail.ru>
2023-11-14 10:26:10 +0400

    add temp field `content`

--- MeowLib.Domain/DbModels/NotificationEntity/Payload/BaseNotificationPayload.cs
+++ MeowLib.Domain/DbModels/NotificationEntity/Payload/BaseNotificationPayload.cs
@@ -2,4 +2,7 @@
 
 public class BaseNotificationPayload
 {
+    // todo: remove or change this
+    // ReSharper disable once InconsistentNaming
+    public byte __Content { get; init; } = Byte.MinValue;
 }
