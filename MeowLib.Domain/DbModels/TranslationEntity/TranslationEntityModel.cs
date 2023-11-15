7f64f3b7daa29ad6e49f577c81daa925f0e5d01c
BlackSoulHub <mr.asaglaev@mail.ru>
2023-11-15 12:08:31 +0500

    create model for translation entity

---
+++ MeowLib.Domain/DbModels/TranslationEntity/TranslationEntityModel.cs
@@ -0,0 +1,13 @@
+﻿using MeowLib.Domain.DbModels.BookEntity;
+using MeowLib.Domain.DbModels.ChapterEntity;
+using MeowLib.Domain.DbModels.TeamEntity;
+
+namespace MeowLib.Domain.DbModels.TranslationEntity;
+
+public class TranslationEntityModel
+{
+    public int Id { get; init; }
+    public required BookEntityModel Book { get; init; }
+    public required TeamEntityModel Team { get; init; }
+    public required IEnumerable<ChapterEntityModel> Chapters { get; init; }
+}
