af3682ef90072d860213b05999c9fcce5a4a59d8
BlackSoulHub <mr.asaglaev@mail.ru>
2023-11-15 12:21:16 +0500

    create required exception

---
+++ MeowLib.Domain/Exceptions/Translation/TeamAlreadyTranslateBookException.cs
@@ -0,0 +1,4 @@
+﻿namespace MeowLib.Domain.Exceptions.Translation;
+
+public class TeamAlreadyTranslateBookException(int teamId, int bookId) : ApiException(
+    $"Комманда с Id = {teamId} уже занимается переводом книги с Id = {bookId}");
