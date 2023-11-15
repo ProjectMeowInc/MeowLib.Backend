c0ee3ac675f0fc7919ead99444d120209c3e90bc
BlackSoulHub <mr.asaglaev@mail.ru>
2023-11-15 13:17:57 +0500

    add position property

--- MeowLib.Domain/Dto/Chapter/ChapterDto.cs
+++ MeowLib.Domain/Dto/Chapter/ChapterDto.cs
@@ -5,4 +5,5 @@ public class ChapterDto
     public required int Id { get; set; }
     public required string Name { get; set; }
     public required DateTime ReleaseDate { get; set; }
+    public required uint Position { get; set; }
 }
