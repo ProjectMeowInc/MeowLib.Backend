4cf6a206ceea79178a01b49d1ac6fe1e938fce30
BlackSoulHub <mr.asaglaev@mail.ru>
2023-11-19 12:57:55 +0500

    create base coin controller

---
+++ MeowLib.WebApi/Controllers/CoinController.cs
@@ -0,0 +1,37 @@
+﻿using MeowLib.Domain.Enums;
+using MeowLib.Domain.Exceptions.User;
+using MeowLib.Services.Interface;
+using MeowLib.WebApi.Abstractions;
+using MeowLib.WebApi.Filters;
+using MeowLib.WebApi.Models.Requests.Coin;
+using MeowLib.WebApi.ProducesResponseTypes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeowLib.WebApi.Controllers;
+
+[Route("api/coins")]
+public class CoinController(ICoinService coinService, ILogger<CoinController> logger) : BaseController
+{
+    [HttpPost("admin-change")]
+    [Authorization(RequiredRoles = new[] { UserRolesEnum.Admin })]
+    [ProducesOkResponseType]
+    [ProducesUserErrorResponseType]
+    public async Task<IActionResult> UpdateCoinsByAdmin([FromBody] ChangeCoinsByAdminRequest payload)
+    {
+        var changeCoinsResult = await coinService.ChangeCoinsByAdminAsync(payload.UserId, payload.UpdateCoins);
+        if (changeCoinsResult.IsFailure)
+        {
+            var exception = changeCoinsResult.GetError();
+            if (exception is UserNotFoundException)
+            {
+                return Error("Запрашиваемый пользователь не найден.");
+            }
+
+            logger.LogError("Неизвестная ошибка при обновлении монет пользователя администратором: {exception}",
+                exception);
+            return ServerError();
+        }
+
+        return Ok();
+    }
+}
