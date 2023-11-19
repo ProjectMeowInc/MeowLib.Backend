dbaec991eed22b5a8f49d7858fc2023cce5ed7e6
BlackSoulHub <mr.asaglaev@mail.ru>
2023-11-19 13:02:06 +0500

    create request for change coins by admin

---
+++ MeowLib.WebApi/Models/Requests/Coin/ChangeCoinsByAdminRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace MeowLib.WebApi.Models.Requests.Coin;
+
+public class ChangeCoinsByAdminRequest
+{
+    public required int UserId { get; init; }
+    public required decimal UpdateCoins { get; init; }
+}
