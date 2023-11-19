5c0e7c8d8fc75025b745bce7f6f802d675f54a05
BlackSoulHub <mr.asaglaev@mail.ru>
2023-11-19 13:01:26 +0500

    create create coins change reason enum

--- MeowLib.Domain/DbModels/CoinsChangeLogEntity/CoinsChangeLogEntityModel.cs
+++ MeowLib.Domain/DbModels/CoinsChangeLogEntity/CoinsChangeLogEntityModel.cs
@@ -7,7 +7,7 @@ public class CoinsChangeLogEntityModel
 {
     public int Id { get; init; }
     public required decimal Value { get; init; }
-    public required MoneyChangeReasonTypeEnum Type { get; init; }
+    public required CoinsChangeReasonTypeEnum Type { get; init; }
     public required DateTime Date { get; init; }
     public required UserEntityModel User { get; init; }
 }
