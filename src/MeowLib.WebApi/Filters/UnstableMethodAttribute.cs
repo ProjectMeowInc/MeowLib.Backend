f373c919aab5768563130ccb778010e43486f3e0
BlackSoulHub <mr.asaglaev@mail.ru>
2023-12-17 15:54:59 +0500

    add unstable method attribute

---
+++ src/MeowLib.WebApi/Filters/UnstableMethodAttribute.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace MeowLib.WebApi.Filters;
+
+public class UnstableMethodAttribute : Attribute, IAsyncActionFilter
+{
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        var logger = context.HttpContext.RequestServices.GetRequiredService<ILogger<UnstableMethodAttribute>>();
+        logger.LogWarning("Использован нестабильный метод");
+        context.HttpContext.Response.Headers.Append("Method-State", "Unstable");
+        await next();
+    }
+}
