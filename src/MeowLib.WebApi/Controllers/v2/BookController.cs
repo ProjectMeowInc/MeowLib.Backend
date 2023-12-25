085a7de3421e561cc57e3aecd7701f0cfff1e153
BlackSoulHub <mr.asaglaev@mail.ru>
2023-12-25 07:27:06 +0500

    create v2 book controller

---
+++ src/MeowLib.WebApi/Controllers/v2/BookController.cs
@@ -0,0 +1,42 @@
+﻿using MeowLib.Services.Interface;
+using MeowLib.WebApi.Abstractions;
+using MeowLib.WebApi.Filters;
+using MeowLib.WebApi.Models.Responses.v2.Author;
+using MeowLib.WebApi.Models.Responses.v2.Book;
+using MeowLib.WebApi.ProducesResponseTypes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeowLib.WebApi.Controllers.v2;
+
+[Route("api/v2/books")]
+public class BookController(IBookService bookService) : BaseController
+{
+    /// <summary>
+    /// Получение всех книг.
+    /// </summary>
+    [HttpGet]
+    [UnstableMethod]
+    [ProducesOkResponseType(typeof(GetAllBooksResponse))]
+    public async Task<IActionResult> GetAllBooks()
+    {
+        var books = await bookService.GetAllBooksAsync();
+        return Ok(new GetAllBooksResponse
+        {
+            Items = books.Select(b => new ShortBookModel
+                {
+                    Id = b.Id,
+                    Name = b.Name,
+                    Description = b.Description,
+                    ImageUrl = b.ImageName,
+                    Author = b.Author != null 
+                        ? new AuthorShortModel
+                        {
+                            Id = b.Author.Id,
+                            Name = b.Author.Name
+                        }
+                        : null
+                })
+                .ToList()
+        });
+    }
+}
