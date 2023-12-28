69ac0c49d5920fad01d85aa8cdc978f7e961f3dc
BlackSoulHub <mr.asaglaev@mail.ru>
2023-12-28 22:58:53 +0500

    create file exceptions

---
+++ src/MeowLib.Domain/Exceptions/File/FileHasIncorrectExtensionException.cs
@@ -0,0 +1,13 @@
+﻿namespace MeowLib.Domain.Exceptions.File;
+
+/// <summary>
+/// Класс исключений, когда файл имеет некорректное расширение.
+/// </summary>
+public class FileHasIncorrectExtensionException(string errorMessage, string currentExtension)
+    : ApiException(errorMessage)
+{
+    /// <summary>
+    /// Расширение, которое имел файл.
+    /// </summary>
+    public string CurrentExtension { get; protected init; } = currentExtension;
+}
