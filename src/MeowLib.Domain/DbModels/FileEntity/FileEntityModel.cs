62d8829621fee991bda7182621f0d584aa9f8f31
BlackSoulHub <mr.asaglaev@mail.ru>
2023-12-28 22:57:25 +0500

    create file entity

---
+++ src/MeowLib.Domain/DbModels/FileEntity/FileEntityModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MeowLib.Domain.DbModels.FileEntity;
+
+public class FileEntityModel
+{
+    public int Id { get; init; }
+    public required string FileSystemName { get; init; }
+    public required DateTime UploadAt { get; init; }
+}
