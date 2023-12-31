3e79e284065ce84af1db641e0ac3a20994e53fc5
blacksoulhub <mr.asaglaev@mail.ru>
2023-12-31 20:46:32 +0500

    move models to entity

--- src/MeowLib.Domain/Exceptions/Translation/TranslationNotFoundException.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace MeowLib.Domain.Exceptions.Translation;
-
-public class TranslationNotFoundException
-    (int translationId) : ApiException($"Перевод с Id = {translationId} не найден");
---
+++ src/MeowLib.Domain/Translation/Exceptions/TranslationNotFoundException.cs
@@ -0,0 +1,4 @@
+﻿namespace MeowLib.Domain.Exceptions.Translation;
+
+public class TranslationNotFoundException(int translationId)
+    : ApiException($"Перевод с Id = {translationId} не найден");
