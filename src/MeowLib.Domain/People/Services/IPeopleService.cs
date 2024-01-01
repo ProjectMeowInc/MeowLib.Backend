74cdec409ccc1421572580cef7cdb1a8baba5a3d
blacksoulhub <mr.asaglaev@mail.ru>
2024-01-01 21:34:48 +0500

    update people service docs

--- src/MeowLib.Domain/People/Services/IPeopleService.cs
+++ src/MeowLib.Domain/People/Services/IPeopleService.cs
@@ -1,6 +1,5 @@
 using MeowLib.Domain.People.Dto;
 using MeowLib.Domain.People.Entity;
-using MeowLib.Domain.Shared;
 using MeowLib.Domain.Shared.Exceptions.Services;
 using MeowLib.Domain.Shared.Result;
 
@@ -12,48 +11,55 @@ namespace MeowLib.Domain.People.Services;
 public interface IPeopleService
 {
     /// <summary>
-    /// Метод создаёт нового автора.
+    /// Метод создаёт нового человека.
     /// </summary>
-    /// <param name="name">Имя автора.</param>
-    /// <returns>DTO-модель автора.</returns>
+    /// <param name="name">Имя человека.</param>
+    /// <returns>DTO-модель человека.</returns>
     /// <exception cref="ValidationException">Возникает в случае ошибки валидации данных.</exception>
-    Task<Result<PeopleEntityModel>> CreateAuthorAsync(string name);
+    Task<Result<PeopleEntityModel>> CreatePeopleAsync(string name);
 
     /// <summary>
-    /// Метод получает всех авторов.
+    /// Метод получает всех людей.
     /// </summary>
-    /// <returns>DTO список авторов.</returns>
-    Task<IEnumerable<PeopleDto>> GetAllAuthorsAsync();
+    /// <returns>DTO список людей.</returns>
+    Task<IEnumerable<PeopleDto>> GetAllPeoplesAsync();
 
     /// <summary>
-    /// Метод обновляет информацию об авторе.
+    /// Метод получает всех людей с разбиением на страницы.
     /// </summary>
-    /// <param name="id">Id автора.</param>
+    /// <param name="perPageCount">Количество людей на странице.</param>
+    /// <param name="page">Номер страницы. Начиная с 0.</param>
+    /// <returns>Найденные люди.</returns>
+    Task<List<PeopleEntityModel>> GetAllPeoplesWithPageAsync(int perPageCount, int page);
+
+    /// <summary>
+    /// Метод обновляет информацию об человеке.
+    /// </summary>
+    /// <param name="id">Id человека.</param>
     /// <param name="data">Данные для обновления.</param>
-    /// <returns>Обновлённую модель данных.</returns>
+    /// <returns>Обновлённую модель человека.</returns>
     /// <exception cref="ValidationException">Возникает в случае, если введёные данные некорректны.</exception>
-    Task<Result<PeopleEntityModel?>> UpdateAuthorAsync(int id, PeopleDto data);
+    Task<Result<PeopleEntityModel?>> UpdatePeopleAsync(int id, PeopleDto data);
 
     /// <summary>
-    /// Метод удаляет автора.
+    /// Метод удаляет человека.
     /// </summary>
-    /// <param name="id"></param>
-    /// <returns>True - в случае удачного удаления, false - в случае если автор не найден.</returns>
-    /// <exception cref="ApiException">Возникает в случае внутренней ошибки.</exception>
-    Task<Result<bool>> DeleteAuthorAsync(int id);
+    /// <param name="peopleId">Id человека.</param>
+    /// <returns>True - в случае удачного удаления, false - в случае если человек не найден.</returns>
+    Task<Result<bool>> DeletePeopleAsync(int peopleId);
 
     /// <summary>
-    /// Метод получает автора по его Id.
+    /// Метод получает человека по его Id.
     /// </summary>
-    /// <param name="authorId">Id автора.</param>
+    /// <param name="peopleId">Id автора.</param>
     /// <returns>DTO-модель автора.</returns>
-    Task<PeopleEntityModel?> GetAuthorByIdAsync(int authorId);
+    Task<PeopleEntityModel?> GetPeopleByIdAsync(int peopleId);
 
     /// <summary>
-    /// Метод получает список авторов подходящих под поисковые параметры.
+    /// Метод получает список людей подходящих под поисковые параметры.
     /// </summary>
-    /// <param name="name">Имя автора</param>
-    /// <returns>Список авторов подходящих под параметры поиска.</returns>
-    /// <exception cref="SearchNotFoundException">Возникает если не был найден автор по заданным параметрам поиска.</exception>
-    Task<Result<IEnumerable<PeopleDto>>> GetAuthorWithParams(string? name);
+    /// <param name="name">Имя человека.</param>
+    /// <returns>Список людей подходящих под параметры поиска.</returns>
+    /// <exception cref="SearchNotFoundException">Возникает если не был найден человек по заданным параметрам поиска.</exception>
+    Task<Result<IEnumerable<PeopleDto>>> GetPeopleWithParams(string? name);
 }
