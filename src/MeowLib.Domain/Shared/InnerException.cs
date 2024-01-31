ffdde3d6531ab7c856d3dbf17e3f86a483ad3ed2
BlackSoulHub <mr.asaglaev@mail.ru>
2024-01-31 07:36:28 +0500

    update inner exception

--- src/MeowLib.Domain/Shared/InnerException.cs
+++ src/MeowLib.Domain/Shared/InnerException.cs
@@ -3,7 +3,4 @@
 /// <summary>
 /// Класс исключений, для ошибок связанных с внутренней реализацией (н.р. ошибка сохранения в БД)
 /// </summary>
-public class InnerException : ApiException
-{
-    public InnerException(string errorMessage) : base(errorMessage) { }
-}
+public class InnerException(string errorMessage) : Exception(errorMessage);
